3935cc2f5252308b84b89f715d6450efc0d3e237
DanLloyd <l014135c@student.staffs.ac.uk>
2015-01-29 17:37:52 +0000

    Merge of Ball class

--- WildMonsters/WildMonsters/GameScene.cs
+++ WildMonsters/WildMonsters/GameScene.cs
@@ -16,12 +16,14 @@ namespace WildMonsters
 	public class GameScene : Scene
 	{
 		private LevelManager levelManager;
-		
+		private Ball testBall;
+
 		public GameScene()
 		{
 			Scheduler.Instance.ScheduleUpdateForTarget(this, 1, false);	// Tells the director to call the update function of this "node"
 			
 			levelManager = new LevelManager(this);
+			testBall = new Ball(this);
 		}
 		
 		public override void Update(float deltaTime)
