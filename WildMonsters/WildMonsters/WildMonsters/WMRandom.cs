188e60f79a7f5420e3e9ecb01a6602b08ba771fc
Zac Clennell <c017159c@student.staffs.ac.uk>
2015-02-07 14:16:48 +0000

    Fixed the crashes I think? Was a silly mistake

--- WildMonsters/WildMonsters/WildMonsters/WMRandom.cs
+++ WildMonsters/WildMonsters/WildMonsters/WMRandom.cs
@@ -10,7 +10,7 @@ namespace WildMonsters
 		public static int GetNextInt(int min, int max, int hash)
 		{
 			randomGen = new Random(count + DateTime.Now.Millisecond + hash);
-			count = (int)(hash +randomGen.Next (2,4))/DateTime.Now.Millisecond;
+			count = (int)(hash +randomGen.Next (2,4))/(DateTime.Now.Millisecond+1);
 			return randomGen.Next (min, max);
 		}
 	}
